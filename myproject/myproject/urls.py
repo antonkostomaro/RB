1b2dd7d67f759540c6b6dfda75c0efc418332785
Anton <antonantona101a@gmail.com>
2024-09-19 09:56:26 +0300

    Добовление отображение на главной постов

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import include, path
-
+from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', home, name='home'),
 ]
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import Post
 
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')  # Получаем все посты, отсортированные по дате
-    return render(request, 'home/home.html', {'posts': posts})
+    posts = Post.objects.all()  # Получаем все посты
+    return render(request, 'home.html', {'posts': posts})
